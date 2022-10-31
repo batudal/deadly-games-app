0fa1c818493a8ceea260e1f736bd1c2eece5bde0
takez0_o <takez0_o@protonmail.com>
2022-11-01 00:18:55 +0300

    test

--- src/App.svelte
+++ src/App.svelte
@@ -58,91 +58,106 @@
   });
 </script>
 
-<main>
-  {#if $tweenedProgress < 1}
-    <div
-      transition:fade|local={{
-        duration: 200,
-      }}
-      class="wrapper"
-    >
-      <p class="loading">Loading</p>
-      <div class="bar-wrapper">
-        <div class="bar" style="width: {$tweenedProgress * 100}%" />
-      </div>
-    </div>
-  {/if}
-  <Canvas size={{ height: window.innerHeight, width: window.innerWidth }}>
-    <Fog color={"#444444"} near={25} far={100} />
-    <HTML occlude transform distanceFactor={1.17} position={{ x: -19.6, y: 28, z: -8.25 }} rotation={{ y: -Math.PI / 2 }}>
-      <img
-        src="./invis.gif"
-        alt=""
-        on:click={() => {
-          music = true;
+{#if music}
+  <main>
+    {#if $tweenedProgress < 1}
+      <div
+        style="z-index:100"
+        transition:fade|local={{
+          duration: 200,
         }}
-        on:keydown
-      />
-    </HTML>
-    <HemisphereLight intensity={0.5} skyColor={0xffbd08} groundColor={0x323973} />
+        class="wrapper"
+      >
+        <p class="loading">Loading</p>
+        <div class="bar-wrapper">
+          <div class="bar" style="width: {$tweenedProgress * 100}%" />
+        </div>
+      </div>
+    {/if}
+    <Canvas size={{ height: window.innerHeight, width: window.innerWidth }}>
+      <Fog color={"#444444"} near={20} far={100} />
+      <HTML zIndexRange={[0, 1]} occlude transform distanceFactor={1.17} position={{ x: -19.6, y: 28, z: -8.25 }} rotation={{ y: -Math.PI / 2 }}>
+        <img
+          src="./invis.gif"
+          alt=""
+          on:click={() => {
+            music = true;
+          }}
+          on:keydown
+        />
+      </HTML>
+      <HemisphereLight intensity={0.5} skyColor={0xffbd08} groundColor={0x323973} />
 
-    <!-- <Pass pass={new FilmPass(1000, 0, 0, false)} /> -->
-    <!-- <PointLight visible position={{ x: 30, y: 3, z: 10 }} color={"#44F1A6"} /> -->
-    <PointLight
-      rotation={{ z: -Math.PI / 2 }}
-      power={0.1}
-      receiveShadow
-      shadow
-      decay={4}
-      visible
-      position={{ x: -3, y: 21, z: 3 }}
-      color={"#44F1A6"}
-      intensity={5}
-    />
-
-    {#if streetLight}
+      <Pass pass={new FilmPass(1000, 0, 0, false)} />
+      <!-- <PointLight visible position={{ x: 30, y: 3, z: 10 }} color={"#44F1A6"} /> -->
       <PointLight
         rotation={{ z: -Math.PI / 2 }}
         power={0.1}
         receiveShadow
         shadow
-        decay={4}
+        decay={10}
         visible
-        position={{ x: 25, y: 21, z: 10 }}
-        color={"#C27500"}
-        intensity={5}
+        position={{ x: -3, y: 21, z: 3 }}
+        color={"#44F1A6"}
+        intensity={3}
       />
-    {/if}
 
-    <PerspectiveCamera fov={60} position={{ x: -60, y: 20, z: 60 }} lookAt={{ x: 0, y: 0, z: 0 }}>
-      <OrbitControls autoRotate={false} enableZoom={true} />
-      <AudioListener />
-    </PerspectiveCamera>
-
-    <AmbientLight intensity={0.1} />
-    <Group scale={$scale}>
-      <GLTF castShadow receiveShadow url={"https://decoded-files.fra1.digitaloceanspaces.com/deadly-games/deadly.glb"} scale={3} />
-      {#if music}
-        <PositionalAudio
-          refDistance={10}
-          volume={1}
-          loop
-          autoplay
-          playbackRate="1"
-          directionalCone={{
-            coneInnerAngle: 90,
-            coneOuterAngle: 220,
-            coneOuterGain: 0.3,
-          }}
-          source={"/dilla.mp3"}
-          position={{ x: -15, y: 25, z: 25 }}
-        >
-          <!-- <PositionalAudioHelper /> -->
-        </PositionalAudio>
+      {#if streetLight}
+        <PointLight
+          rotation={{ z: -Math.PI / 2 }}
+          power={0.1}
+          receiveShadow
+          shadow
+          decay={4}
+          visible
+          position={{ x: 25, y: 21, z: 10 }}
+          color={"#C27500"}
+          intensity={5}
+        />
       {/if}
-    </Group>
-  </Canvas>
-</main>
+
+      <PerspectiveCamera fov={60} position={{ x: -60, y: 20, z: 60 }} lookAt={{ x: 0, y: 0, z: 0 }}>
+        <OrbitControls autoRotate={false} enableZoom={true} />
+        <AudioListener />
+      </PerspectiveCamera>
+
+      <AmbientLight intensity={0.1} />
+      <Group scale={$scale}>
+        <GLTF castShadow receiveShadow url={"https://decoded-files.fra1.digitaloceanspaces.com/deadly-games/deadly.glb"} scale={3} />
+        {#if music}
+          <PositionalAudio
+            refDistance={10}
+            volume={2}
+            loop
+            autoplay
+            playbackRate="1"
+            directionalCone={{
+              coneInnerAngle: 150,
+              coneOuterAngle: 270,
+              coneOuterGain: 0.5,
+            }}
+            source={"/dilla.mp3"}
+            position={{ x: -15, y: 25, z: 25 }}
+          >
+            <!-- <PositionalAudioHelper /> -->
+          </PositionalAudio>
+          <Audio source={"/crowd.mp3"} loop autoplay playbackRate="1" volume={0.1} />
+
+          <Audio source={"/bg.mp3"} loop autoplay playbackRate="1" volume={0.7} />
+        {/if}
+      </Group>
+    </Canvas>
+  </main>
+{:else}
+  <div
+    on:click={() => {
+      music = true;
+    }}
+    on:keydown
+  >
+    click
+  </div>
+{/if}
 
 <style>
   main {
